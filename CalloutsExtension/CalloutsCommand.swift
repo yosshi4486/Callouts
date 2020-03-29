bd742ddb939b0bda74514140f35e02626a36784b
yosshi4486 <yoshiharuseijin@gmail.com>
2020-03-29 13:22:20 +0900

    Add AttentionCommand

---
+++ CalloutsExtension/CalloutsCommand.swift
@@ -0,0 +1,20 @@
+//
+//  SourceEditorCommand.swift
+//  CalloutsExtension
+//
+//  Created by yosshi4486 on 2020/03/29
+//  Copyright © 2020 RAISO. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class CalloutsCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
