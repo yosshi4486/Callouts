a5eac8fe5979ed4391fe0e1e73c98280d4995115
yosshi4486 <yoshiharuseijin@gmail.com>
2020-03-29 10:16:13 +0900

    Initial Commit

---
+++ Callouts/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Callouts
+//
+//  Created by yosshi4486 on 2020/03/29.
+//  Copyright © 2020 RAISO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
